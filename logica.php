dead2f078b7dffbeb692f97d5f1de394ea041121
php 01 <php.01@daphne.cec.ime.usp.br>
2019-01-29 21:59:40 -0200

    programa PIN

--- logica.php
+++ logica.php
@@ -1,12 +1,12 @@
 <?php
   for ($i = 1; $i <=100; $i++){
 
-    if ( $i % 2 == 0) {
-      echo $i, " é par **" . PHP_EOL;
+    if ( $i % 4 == 0) {
+      echo "PIN" . PHP_EOL;
     }
     else {
-      echo $i, " é ímpar *" . PHP_EOL;
+      echo $i . PHP_EOL;
     }
-    
+
   }
 ?>
