e021c52e362d0c63133590036fe600a684017a2b
php 01 <php.01@daphne.cec.ime.usp.br>
2019-01-29 21:57:08 -0200

    programa pares e impares

--- logica.php
+++ logica.php
@@ -1,6 +1,12 @@
 <?php
   for ($i = 1; $i <=100; $i++){
 
-    echo $i . PHP_EOL;
+    if ( $i % 2 == 0) {
+      echo $i, " é par **" . PHP_EOL;
+    }
+    else {
+      echo $i, " é ímpar *" . PHP_EOL;
+    }
+    
   }
 ?>
